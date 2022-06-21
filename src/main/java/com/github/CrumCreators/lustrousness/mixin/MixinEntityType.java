6846b301ac0f17605325fd0f692f86d9b79313f7
QPCrummer <66036033+QPCrummer@users.noreply.github.com>
2022-06-20 23:09:01 -0400

    Randomized Mob Health!
    (Currently only works with sheep)

--- src/main/java/com/github/CrumCreators/lustrousness/mixin/MixinEntityType.java
+++ src/main/java/com/github/CrumCreators/lustrousness/mixin/MixinEntityType.java
@@ -1,10 +1,8 @@
 package com.github.CrumCreators.lustrousness.mixin;
 
-import com.github.CrumCreators.lustrousness.utils.RNG;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.NbtCompound;
@@ -17,17 +15,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
-import static com.github.CrumCreators.lustrousness.utils.RNG.rngout;
+import java.util.Objects;
+
+import static net.minecraft.entity.attribute.EntityAttributes.GENERIC_MAX_HEALTH;
 
 @Mixin(EntityType.class)
 public abstract class MixinEntityType <T extends Entity> {
 
-
-
     @Inject(method="create(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/nbt/NbtCompound;Lnet/minecraft/text/Text;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/entity/SpawnReason;ZZ)Lnet/minecraft/entity/Entity;", at=@At(value="INVOKE", target="Lnet/minecraft/entity/mob/MobEntity;playAmbientSound()V", shift= At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
     private void create(ServerWorld world, NbtCompound itemNbt, Text name, PlayerEntity player, BlockPos pos, SpawnReason spawnReason, boolean alignPosition, boolean invertY, CallbackInfoReturnable<T> cir, Entity entity) {
-        RNG.randomize();
-        ((MobEntity) entity).createMobAttributes().add(EntityAttributes.GENERIC_MAX_HEALTH, rngout).add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.23000000417232513D);
+        int health = (60-world.random.nextBetween(10, 50));
+        Objects.requireNonNull(((MobEntity) entity).getAttributeInstance(GENERIC_MAX_HEALTH)).setBaseValue(health);
+        ((MobEntity) entity).setHealth(health);
+            System.out.println(health);
     }
 }
 
