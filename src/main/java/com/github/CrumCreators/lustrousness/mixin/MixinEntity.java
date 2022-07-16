d69fa564f0a130f7d7ef40cee4952eab9174c77b
Ampflower 🌺 <git@ampflower.gay>
2022-07-15 22:55:36 -0500

    Implement bouncy slime boots logic and no fall damage

---
+++ src/main/java/com/github/CrumCreators/lustrousness/mixin/MixinEntity.java
@@ -0,0 +1,30 @@
+package com.github.CrumCreators.lustrousness.mixin;// Created 2022-15-07T21:11:48
+
+import com.github.CrumCreators.lustrousness.Main;
+import com.github.CrumCreators.lustrousness.item.LustItems;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.BlockView;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+/**
+ * @author Ampflower
+ * @since ${version}
+ **/
+@Mixin(Entity.class)
+public class MixinEntity {
+    @Redirect(method = "move",
+            at = @At(value = "INVOKE",
+                    target = "Lnet/minecraft/block/Block;onEntityLand(Lnet/minecraft/world/BlockView;Lnet/minecraft/entity/Entity;)V"))
+    private void lustrousness$onEntityLand$slimeBoots(Block instance, BlockView world, Entity entity) {
+        if (entity instanceof LivingEntity livingEntity && livingEntity.getEquippedStack(EquipmentSlot.FEET).isOf(LustItems.SLIME_BOOTS)) {
+            Main.bounce(livingEntity);
+        } else {
+            instance.onEntityLand(world, entity);
+        }
+    }
+}
